ae5b6693e721cdbd83dc5c28d1c21afd1eff1f9d
Suryapavanlakamsani <142097019+Suryapavanlakamsani@users.noreply.github.com>
2024-10-03 15:29:53 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -19,7 +19,6 @@ function App() {
   return (
     <div className="App">
       {/* Wrap your app with AuthProvider */}
-      <Home/>
       <Router>
         <Routes>
           <Route exact path="/" element={<Home />} />
